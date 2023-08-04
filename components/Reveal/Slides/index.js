627bafcf035781f5cc01a7ce23fdd6a6a51e04a5
Julia Lim <jiay.21@ichat.sp.edu.sg>
2023-08-04 15:04:43 +0800

    feat: small changes to code formatting

--- components/Reveal/Slides/index.js
+++ components/Reveal/Slides/index.js
@@ -1,39 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { Button } from "@chakra-ui/react";
-import PropTypes from 'prop-types'
-import { useRevealDeck } from '..'
+import React, { useEffect, useState } from "react";
+import { Button, Stack } from "@chakra-ui/react";
+import PropTypes from "prop-types";
+import { useRevealDeck } from "..";
 
 export default function Slides({ children, nextPhase }) {
-  const [Deck, { progress, indices }] = useRevealDeck()
+  const [Deck, { progress, indices }] = useRevealDeck();
   const [isButtonVisible, setIsButtonVisible] = useState(false);
 
   useEffect(() => {
     if (progress === 1) {
       setIsButtonVisible(true);
     }
-  }, [progress])
+  }, [progress]);
 
   return (
     <>
       <div className="slides">{children}</div>
-      {isButtonVisible && (
-        <Button 
-        onClick={nextPhase}
-        position="absolute"
-        bottom="2rem"
-        left="50%"
-        transform="translateX(-50%)">
-          Next Phase
-        </Button>
-      )}
     </>
-  )
+  );
 }
 
 Slides.propTypes = {
-  children: PropTypes.any
-}
+  children: PropTypes.any,
+};
 
 Slides.defaultProps = {
-  children: null
-}
+  children: null,
+};
--- components/slides/slide_intro.js
+++ components/slides/slide_intro.js
@@ -1,46 +1,49 @@
 /**
- * 
+ * @deprecated
+ *
  * Moving to no-code slides, hence would not require use of Reveal slide controls
  * To edit slide deck: https://slides.com/teamazen/palette
- * 
+ *
  */
 // import Reveal from 'reveal.js';
 // import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-import '/node_modules/reveal.js/dist/reveal.css';
-import '/node_modules/reveal.js/dist/theme/white.css';
+import "/node_modules/reveal.js/dist/reveal.css";
+import "/node_modules/reveal.js/dist/theme/white.css";
 // import { useEffect } from 'react';
-import React, { useCallback, useEffect } from 'react'
-import { RevealMain, Section, Slides, useRevealDeck } from '../Reveal'
+import React, { useCallback } from "react";
+import { Box } from "@chakra-ui/react";
+import { useRevealDeck } from "../Reveal";
 
 // slide component for slide section before first activity
 export default function SlideIntro(){
   const [Deck, { progress }] = useRevealDeck()
 
   const handleClick = useCallback(() => {
-    if (Deck){
-      Deck.right()
+    if (Deck) {
+      Deck.right();
     }
-
-  }, [Deck])
+  }, [Deck]);
 
   return (
     <>
       <section>
-        <h1>Game Info and Explanation</h1>
+        <h1>Ludos Financial Literacy</h1>
       </section>
       <section>
-          <h2>Game Info</h2>
-          <p>
-            Rules blab bla bla
-          </p>
-        </section>
-        <section>
-          <h2>Game Rules</h2>
-          <p>
-            yap yap yap ypa
-          </p>
+        {/* Render the image using the Box component */}
+        <Box
+          // width="100vw"
+          height="100vh"
+          backgroundImage="url('/img/slide1.png')"
+          backgroundRepeat="no-repeat"
+        >
+          <h1>
+            <strong>Financial Literacy</strong>
+          </h1>
+
+          <strong>Let's Play!</strong>
+        </Box>
       </section>
     </>
-    
-  )
+  );
 }
